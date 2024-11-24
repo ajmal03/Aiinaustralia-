bdf057371d460aa6092c792b497d5317d8504700
Ajmal <52492578+ajmal03@users.noreply.github.com>
2024-11-25 01:24:55 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,16 @@
+ $("#submit-form").submit((e) => {
+      e.preventDefault(); // Prevent page reload
+      $.ajax({
+        url: "https://docs.google.com/forms/u/5/d/e/1FAIpQLSfwvpykhZC-cGAea6dkZH_bpL5vogA15JzS6llhXdKq9_eaSw/formResponse?pli=1", // Replace with your form's URL
+        data: $("#submit-form").serialize(), // Serialize form data
+        method: "POST",
+        success: function (response) {
+          alert("Form submitted successfully!");
+          window.location.reload(); // Reload the page
+          // or redirect to another page: window.location.href = "https://google.com";
+        },
+        error: function (err) {
+          alert("Something went wrong.");
+        },
+      });
+    });
