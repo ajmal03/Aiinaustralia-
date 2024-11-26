f99440f64e210b9d11d69db95662312c711efd38
Ajmal <52492578+ajmal03@users.noreply.github.com>
2024-11-26 12:38:12 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,7 +1,7 @@
  $("#submit-form").submit((e) => {
       e.preventDefault(); // Prevent page reload
       $.ajax({
-        url: "https://docs.google.com/forms/u/5/d/e/1FAIpQLSfwvpykhZC-cGAea6dkZH_bpL5vogA15JzS6llhXdKq9_eaSw/formResponse?pli=1", // Replace with your form's URL
+        url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSc_dLFwDBOwKbgGbqYMWmw29AwMUKozmawL41iSo0j4X7BWWA/formResponse", // Replace with your form's URL
         data: $("#submit-form").serialize(), // Serialize form data
         method: "POST",
         success: function (response) {
