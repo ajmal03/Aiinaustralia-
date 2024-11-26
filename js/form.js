574ca6c9866e581dca6e6cf63573b79174fcb974
Ajmal <52492578+ajmal03@users.noreply.github.com>
2024-11-26 12:58:00 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -10,7 +10,7 @@
           // or redirect to another page: window.location.href = "https://google.com";
         },
         error: function (err) {
-          alert(" Thank You! Your form has been successfully submitted. We will get in touch with you shortly.");
+          alert(" Thank You! We will get in touch with you shortly.");
           window.location.reload();
         },
       });
